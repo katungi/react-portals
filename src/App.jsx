420e41792b24d223f34c65be2b41dc04ae5761a5
katungi <dankatdennis@gmail.com>
2023-10-14 09:26:42 +0300

    identify the problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Modal from './Modal.jsx'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [modalOpen, setModalOpen] = useState(false)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className='relative w-full mb-10' style={{zIndex: 1}}> 
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => setModalOpen(true)}>Open Modal</button>
+        <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+          <p>Modal Content</p>
+        </Modal>
+     </div>
+     <div className='relative bg-purple-600 p-3' style={{zIndex: 2}}>
+        <p>We must be seen</p>
+     </div>
     </>
   )
 }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,37 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 
 export default function Modal({open, children, onClose}) {
  if(!open) return null;
-
-const MODAL_STYLES = {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#FFF',
-  padding: '50px',
-  zIndex: 100000
-}
-
-const OVERLAY_STYLES = {
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, .7)',
-  zIndex: 100000
-}
-
-  return ReactDOM.createPortal(
+  return(
     <>
-    <div style={OVERLAY_STYLES}/>
-    <div style={MODAL_STYLES}>
+    <div className='fixed top-0 left-0 right-0 bottom-0' style={{
+      zIndex: 1000,
+      backgroundColor: 'rgba(0, 0, 0, .7)',
+    }} />
+    <div className='fixed top-1/2 left-1/2 p-12 bg-white' style={{zIndex: 1000,  transform: 'translate(-50%, -50%)'}}>
         <button onClick={onClose}>Close Modal</button>
         {children}
     </div>
-    </>,
-    document.getElementById('portal')
+    </>
   )
 }
